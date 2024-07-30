c4a2cebd9df9c8415ecbba6c5b28ed744abe5f06
iftiar163 <iftiar.hossain163@gmail.com>
2024-07-31 00:14:32 +0600

    final touch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 		<>
 			<header>
 				{/* Desktop Version */}
-				<div className="lg:w-[1280px] md:w-[768px] sm:w-[425px] mx-auto grid grid-cols-3 py-5 items-center">
+				<div className="desktop-header lg:w-[1280px] md:w-[768px] sm:w-[425px] mx-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 py-5 lg:px-0 md:px-4 sm:px-4 items-center">
 					<div className="logo">
 						<Link to={"/"}>
 							<img
@@ -15,7 +15,7 @@ const Header = () => {
 							/>
 						</Link>
 					</div>
-					<div className="main-menu text-center text-white">
+					<div className="main-menu text-center text-white lg:block md:hidden sm:hidden">
 						<ul className=" flex gap-8 justify-center align-middle">
 							<li>
 								<Link to={"/"}>Home</Link>
@@ -34,8 +34,43 @@ const Header = () => {
 							</li>
 						</ul>
 					</div>
-					<div className="login-button text-right">
-						<button className="text-white">Sign Up</button>
+					<div className="login-button">
+						<div className="signup-button flex justify-end">
+							<button className="text-white lg:block md:hidden sm:hidden">
+								Sign Up
+							</button>
+						</div>
+						<div className="nav-button flex justify-end">
+							<Link className="lg:hidden md:block sm:block">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="60"
+									height="60"
+									viewBox="0 0 60 60"
+									fill="none"
+								>
+									<rect width="60" height="60" rx="14" fill="#FE6927" />
+									<path
+										d="M19 21.7856H41"
+										stroke="white"
+										stroke-width="1.96429"
+										stroke-linecap="round"
+									/>
+									<path
+										d="M19 30.4285H41"
+										stroke="white"
+										stroke-width="1.96429"
+										stroke-linecap="round"
+									/>
+									<path
+										d="M19 39.0713H41"
+										stroke="white"
+										stroke-width="1.96429"
+										stroke-linecap="round"
+									/>
+								</svg>
+							</Link>
+						</div>
 					</div>
 				</div>
 			</header>
